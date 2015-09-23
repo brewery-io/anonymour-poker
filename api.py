222b9be74a1b5915d968bd22d7ba5d70fc3abc5b
Rafal <stapinskirafal@gmail.com>
2015-09-23 18:23:59 -0400

    Register class finished

--- api.py
+++ api.py
@@ -3,18 +3,22 @@ from pymongo import MongoClient
 import time
 import datetime
 import json
+from config import Config
+import pysqlw
+import hashlib
 
 urls = (
-    "/register", "register"
+    "/register", "register",
+    "/login", "login"
 )
-client = MongoClient("localhost", 27017)
-db = client["poker"]
-requests = db["requests"]
-users = db["users"]
+# client = MongoClient("localhost", 27017)
+# db = client["poker"]
+# requests = db["requests"]
+# users = db["users"]
 
 def write(payload, status):
     payload["status"] = status
-    return json.dumps(payload)
+    return json.dumps({"payload": payload, "status": status})
 
 def notfound():
     # web.header("Content-Type", "application/json; charset=UTF-8")
@@ -28,8 +32,14 @@ def new_request(request):
     # request_info["ip"] = web.ctx["ip"]
     # request_info["type"] = "POST"
     # request_info["time"] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    #
     # request_id = requests.insert_one(request_info).inserted_id
+
+class login:
+    def POST(self):
+        new_request(self)
+        data = web.input()
+        username = data["username"]
+        password = data["password"]
 
 class register:
     def POST(self):
@@ -37,19 +47,44 @@ class register:
         data = web.input()
         username = data["username"]
         password = data["password"]
+        password_hash = hashlib.sha224(password).hexdigest()
 
         try:
             username.decode("utf-8")
             password.decode("utf-8")
         except UnicodeError:
-            return write({"payload": {"error": "Username or password not UTF-8 encoded. "}}, 210)
+            return write({"error": "Username or password not UTF-8 encoded. "}, 210)
 
         if len(username) < 3:
-            return write({"payload": {"error": "Username must be at least 3 characters long. "}}, 210)
+            return write({"error": "Username must be at least 3 characters long. "}, 210)
         elif len(username) > 20:
-            return write({"payload": {"error": "Username must be at most 20 characters long. "}}, 210)
+            return write({"error": "Username must be at most 20 characters long. "}, 210)
         elif len(password) > 100:
-            return write({"payload": {"error": "Password must be at most 100 characters long. "}}, 210)
+            return write({"error": "Password must be at most 100 characters long. "}, 210)
+        elif username == "" or password == "":
+            return write({"error": "Username or password can not be empty. "}, 210)
+
+        db = pysqlw.pysqlw(**{
+            "db_type": Config.db_type,
+            "db_host": Config.db_host,
+            "db_user": Config.db_user,
+            "db_pass": Config.db_pass,
+            "db_name": Config.db_name
+        })
+
+        user = db.where("username", db.escape(username)).get("users")
+
+        if user != ():
+            return write({"error": "Username already exists. "}, 211)
+
+        inserted = db.insert("users", {"username": db.escape(username), "password": password_hash, "balance": 0})
+
+        db.close()
+
+        if inserted:
+            return write({"message": "Successfully registered %s. " % username}, 200)
+        else:
+            return write({"error": "Unable to register. "}, 500)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
