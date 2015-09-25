98bee5698a2dd97819b06b8b1a53a869a0c5464f
Rafal <stapinskirafal@gmail.com>
2015-09-25 16:53:56 -0400

    Create room method finished

---
+++ api/modules/room.py
@@ -0,0 +1,26 @@
+import getopt
+import sys
+import json
+
+def start_game(players, max_players):
+    
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv, "p:m:", ["players=", "max="])
+        for opt in opts:
+            if opt[0] == "-m":
+                max_players = opt[1]
+            elif opt[0] == "-p":
+                players = opt[1]
+
+        start_game(players, max_players)
+
+
+    except getopt.GetoptError:
+        print "error"
+        sys.exit()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
