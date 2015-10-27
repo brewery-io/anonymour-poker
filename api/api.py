6044a4376ab0bd2e9f05f5d90d65babc06358714
brewery-io <rafal@stapinski.co>
2015-10-27 13:31:22 -0400

    Updates

--- api/api.py
+++ api/api.py
@@ -8,7 +8,21 @@ import pysqlw
 import hashlib
 import os
 
-db = pysqlw.pysqlw(**{
+#########################################################
+#
+#               INITIAL SETTINGS
+#
+#########################################################
+
+urls = (
+    "/user/register", "user_register",
+    "/user/login", "user_login",
+    "/room/create", "room_create",
+    "/game/start", "game_start",
+    "/game/stop", "game_stop "
+)
+
+mysql_db = pysqlw.pysqlw(**{
     "db_type": Config.db_type,
     "db_host": Config.db_host,
     "db_user": Config.db_user,
@@ -16,20 +30,21 @@ db = pysqlw.pysqlw(**{
     "db_name": Config.db_name
 })
 
-urls = (
-    "/register", "register",
-    "/login", "login"
-)
-# client = MongoClient("localhost", 27017)
-# db = client["poker"]
-# requests = db["requests"]
-# users = db["users"]
+mongo_client = MongoClient("localhost", 27017)
+mongo_db = mongo_client["poker"]
+rooms_doc = mongo_db["rooms"]
+
+#########################################################
+#
+#               SUPPORT METHODS
+#
+#########################################################
 
 def write(payload, status):
     payload["status"] = status
     return json.dumps({"payload": payload, "status": status})
 
-def notfound():
+def notfound(self):
     # web.header("Content-Type", "application/json; charset=UTF-8")
     new_request(self)
     return web.notfound('{"payload": {"error_message": "Endpoint not found. "}, "status": 404}')
@@ -43,7 +58,68 @@ def new_request(request):
     # request_info["time"] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     # request_id = requests.insert_one(request_info).inserted_id
 
-class login:
+#########################################################
+#
+#               GAME CLASSES
+#
+#########################################################
+
+class game_start:
+    def POST(self):
+        new_request(self)
+        data = web.input()
+        print "first print"
+        time.sleep(10)
+        print "second print"
+
+#########################################################
+#
+#               ROOM CLASSES
+#
+#########################################################
+
+class room_create:
+    def POST(self):
+        new_request(self)
+        data = web.input()
+        if data["hash"]:
+            hash = data["hash"]
+
+        user_session = mysql_db.where("hash", mysql_db.escape(hash)).get("users_sessions")
+
+        if user_session == ():
+            return write({"error": "Session doesn't exist ."}, 211)
+        else:
+            user = mysql_db.where("id", user_session[0]["user_id"]).get("users")
+            permissions = json.loads(user[0]["permissions"])
+            if permissions["Admin"] == 1:
+                if data["name"] and data["max_players"] and data["buyin"]:
+                    name = mysql_db.escape(data["name"])
+                    max_players = mysql_db.escape(data["max_players"])
+                    buyin = mysql_db.escape(data["buyin"])
+                    inserted = mysql_db.insert("rooms", {"name": name, "max_players": max_players, "buyin": buyin})
+                    room = {
+                        "name": name,
+                        "max_players": max_players,
+                        "buyin": buyin
+                    }
+                    room_id = rooms_doc.insert_one()
+                    if inserted:
+                        return write({"message": "Room created successfully. "}, 200)
+                    else:
+                        return write({"error": "Error inserting into database. "}, 212)
+                else:
+                    return write({"error": "Name not supplied. "}, 210)
+            else:
+                return write({"error": "You don't have permission to do this. "}, 211)
+
+#########################################################
+#
+#               USER CLASSES
+#
+#########################################################
+
+class user_login:
     def POST(self):
         new_request(self)
         data = web.input()
@@ -56,18 +132,21 @@ class login:
 
         hash = os.urandom(32).encode("hex")
 
-        user = db.where("username", db.escape(username)).get("users")
+        user = mysql_db.where("username", mysql_db.escape(username)).get("users")
 
         if user == ():
             return write({"error": "Username not found. "}, 211)
         elif user[0]["password"] == password_hash:
-            db.insert("users_sessions", {"user_id": user[0]["id"], "hash": hash})
-            return write({"message": "Successfully logged in. ", "hash": hash}, 200)
+            inserted = mysql_db.insert("users_sessions", {"user_id": user[0]["id"], "hash": hash})
+            if inserted:
+                return write({"message": "Successfully logged in. ", "hash": hash}, 200)
+            else:
+                return write({"error": "Error inserting into database. "}, 212)
         else:
             return write({"error": "Password was incorrect. "}, 211)
 
 
-class register:
+class user_register:
     def POST(self):
         new_request(self)
         data = web.input()
@@ -93,20 +172,26 @@ class register:
         elif username == "" or password == "":
             return write({"error": "Username or password can not be empty. "}, 210)
 
-        user = db.where("username", db.escape(username)).get("users")
+        user = mysql_db.where("username", mysql_db.escape(username)).get("users")
 
         if user != ():
             return write({"error": "Username already exists. "}, 211)
 
-        inserted = db.insert("users", {"username": db.escape(username), "password": password_hash, "permissions": "{\"Standard\": 1, \"Admin\": 0}", "balance": 0})
+        inserted = mysql_db.insert("users", {"username": mysql_db.escape(username), "password": password_hash, "permissions": "{\"Standard\": 1, \"Admin\": 0}", "balance": 0})
 
         if inserted:
             return write({"message": "Successfully registered %s. " % username}, 200)
         else:
-            return write({"error": "Unable to register. "}, 500)
+            return write({"error": "Error inserting into database. "}, 212)
+
+#########################################################
+#
+#               INIT STATEMENTS
+#
+#########################################################
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.notfound = notfound
     app.run()
-    db.close()
+    mysql_db.close()
---
+++ res/js/loggedin.js
@@ -0,0 +1,5 @@
+if (!Cookies.get("hash")) {
+    window.location.href = "login.html";
+} else {
+    // window.location.href = "home.html";
+}
